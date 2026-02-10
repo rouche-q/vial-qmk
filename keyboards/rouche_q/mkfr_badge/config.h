af0975b25315b54ee906dcbd508f0a3a20e34333
rouche-q <17704187+rouche-q@users.noreply.github.com>
2026-02-10 22:30:48 +0100

    feat(keyboard): add mkfr badge firmware

--- keyboards/rouche_q/mkfr_badge/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#undef I2C_DRIVER
-#define I2C_DRIVER I2CD1
-#undef I2C1_SCL_PIN
-#define I2C1_SCL_PIN GP3
-#undef I2C1_SDA_PIN
-#define I2C1_SDA_PIN GP2
-#define OLED_TIMEOUT 120000
